9ea604cdd8dc723a488e43b5e96d33f918302aea
Ivan Ponomarev <iponomarev@mail.ru>
2021-10-29 22:06:36 +0300

    static lock & debug ouptut

--- src/main/java/com/github/fppt/jedismock/server/RedisOperationExecutor.java
+++ src/main/java/com/github/fppt/jedismock/server/RedisOperationExecutor.java
@@ -11,12 +11,14 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by Xiaolu on 2015/4/20.
  */
 public class RedisOperationExecutor {
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(RedisOperationExecutor.class);
+    private static final Object lock = new Object();
     private final OperationExecutorState state;
     private final CombinedOperationFactory operationFactory = new CombinedOperationFactory();
 
@@ -32,27 +34,30 @@ public class RedisOperationExecutor {
         String name = new String(params.get(0).data()).toLowerCase();
 
         //No parallel execution
-        state.getLock().lock();
-        try {
-            //Checking if we are affecting the server or client state.
-            //This is done outside the context of a transaction which is why it's a separate check
-            Optional<RedisOperation> result = operationFactory.buildMetaOperation(name, state, commandParams);
-            if (result.isPresent()) return result.get().execute();
+        synchronized (lock) {
+            System.out.printf("(%s)>%s %s%n",
+                    Thread.currentThread().getName(),
+                    name,
+                    params.stream().map(Slice::toString).collect(Collectors.joining(" ")));
+            try {
+                //Checking if we are affecting the server or client state.
+                //This is done outside the context of a transaction which is why it's a separate check
+                Optional<RedisOperation> result = operationFactory.buildMetaOperation(name, state, commandParams);
+                if (result.isPresent()) return result.get().execute();
 
-            //Checking if we mutating the transaction or the redisBases
-            RedisOperation redisOperation = operationFactory.buildTxOperation(name, state.base(), commandParams);
-            if (state.isTransactionModeOn()) {
-                state.tx().add(redisOperation);
-            } else {
-                return Response.clientResponse(name, redisOperation.execute());
+                //Checking if we mutating the transaction or the redisBases
+                RedisOperation redisOperation = operationFactory.buildTxOperation(name, state.base(), commandParams);
+                if (state.isTransactionModeOn()) {
+                    state.tx().add(redisOperation);
+                } else {
+                    return Response.clientResponse(name, redisOperation.execute());
+                }
+
+                return Response.clientResponse(name, Response.OK);
+            } catch (UnsupportedOperationException | WrongValueTypeException | IllegalArgumentException e) {
+                LOG.error("Malformed request", e);
+                return Response.error(e.getMessage());
             }
-
-            return Response.clientResponse(name, Response.OK);
-        } catch (UnsupportedOperationException | WrongValueTypeException | IllegalArgumentException e) {
-            LOG.error("Malformed request", e);
-            return Response.error(e.getMessage());
-        } finally {
-            state.getLock().unlock();
         }
     }
 }
