86c03ff1045bcabbe65bb15e04faa23f5ff5ea38
mdvorchik <maxim.dvorchik@yandex.ru>
2022-03-21 23:50:18 +0300

    add ByLex support to ZRange and refactor

--- src/main/java/com/github/fppt/jedismock/operations/sortedsets/ZRange.java
+++ src/main/java/com/github/fppt/jedismock/operations/sortedsets/ZRange.java
@@ -15,25 +15,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.github.fppt.jedismock.Utils.convertToInteger;
-import static com.github.fppt.jedismock.Utils.convertToLong;
 
 @RedisCommand("zrange")
 class ZRange extends AbstractByScoreOperation {
 
+    public static final Comparator<Map.Entry<Slice, Double>> zRangeComparator = Comparator
+            .comparingDouble((ToDoubleFunction<Map.Entry<Slice, Double>>) Map.Entry::getValue)
+            .thenComparing(Map.Entry::getKey);
+
     private static final String WITH_SCORES = "WITHSCORES";
     private static final String IS_REV = "REV";
     private static final String IS_BYSCORE = "BYSCORE";
-    private static final String IS_LIMIT = "LIMIT";
-    private static final Comparator<Map.Entry<Slice, Double>> zRangeComparator = Comparator
-            .comparingDouble((ToDoubleFunction<Map.Entry<Slice, Double>>) Map.Entry::getValue)
-            .thenComparing(Map.Entry::getKey);
+    private static final String IS_BYLEX = "BYLEX";
 
     private boolean withScores = false;
     private boolean isRev = false;
     private boolean isByScore = false;
-    private boolean isLimit = false;
-    private long offset = 0;
-    private long count = 0;
+    private boolean isByLex = false;
     private int start = 0;
     private int end = 0;
 
@@ -48,7 +46,25 @@ class ZRange extends AbstractByScoreOperation {
         final Map<Slice, Double> map = mapDBObj.getStoredData();
         Predicate<Double> filterPredicate = getFilterPredicate(params().get(1).toString(), params().get(2).toString());
 
-        initVars();
+        parseArgs();
+
+        if (isByScore && !isRev) {
+            ZRangeByScore zRangeByScore = new ZRangeByScore(base(), params());
+            return zRangeByScore.response();
+        }
+        if (isByScore) {
+            ZRevRangeByScore zRevRangeByScore = new ZRevRangeByScore(base(), params());
+            return zRevRangeByScore.response();
+        }
+        if (isByLex && !isRev) {
+            ZRangeByLex zRangeByLex = new ZRangeByLex(base(), params());
+            return zRangeByLex.response();
+        }
+        if (isByLex) {
+            ZRevRangeByLex zRevRangeByLex = new ZRevRangeByLex(base(), params());
+            return zRevRangeByLex.response();
+        }
+
         calculateIndexes(map);
 
         boolean finalWithScores = withScores;
@@ -57,8 +73,8 @@ class ZRange extends AbstractByScoreOperation {
         List<Slice> values = map.entrySet().stream()
                 .filter(e -> !finalIsByScore || filterPredicate.test(e.getValue()))
                 .sorted(isRev ? zRangeComparator.reversed() : zRangeComparator)
-                .skip(isLimit ? offset : (isByScore ? 0 : start))
-                .limit(isLimit ? count : (isByScore ? map.entrySet().size() : end - start + 1))
+                .skip(start)
+                .limit(end - start + 1)
                 .flatMap(e -> finalWithScores
                         ? Stream.of(e.getKey(), Slice.create(e.getValue().toString()))
                         : Stream.of(e.getKey()))
@@ -69,31 +85,29 @@ class ZRange extends AbstractByScoreOperation {
     }
 
     private void calculateIndexes(Map<Slice, Double> map) {
-        if (!isByScore) {
-            start = convertToInteger(params().get(1).toString());
-            end = convertToInteger(params().get(2).toString());
+        start = convertToInteger(params().get(1).toString());
+        end = convertToInteger(params().get(2).toString());
 
+        if (start < 0) {
+            start = map.size() + start;
             if (start < 0) {
-                start = map.size() + start;
-                if (start < 0) {
-                    start = 0;
-                }
+                start = 0;
             }
+        }
 
+        if (end < 0) {
+            end = map.size() + end;
             if (end < 0) {
-                end = map.size() + end;
-                if (end < 0) {
-                    end = -1;
-                }
+                end = -1;
             }
+        }
 
-            if (end >= map.size()) {
-                end = map.size() - 1;
-            }
+        if (end >= map.size()) {
+            end = map.size() - 1;
         }
     }
 
-    private void initVars() {
+    private void parseArgs() {
         for (Slice param : params()) {
             if (WITH_SCORES.equalsIgnoreCase(param.toString())) {
                 withScores = true;
@@ -104,11 +118,8 @@ class ZRange extends AbstractByScoreOperation {
             if (IS_BYSCORE.equalsIgnoreCase(param.toString())) {
                 isByScore = true;
             }
-            if (IS_LIMIT.equalsIgnoreCase(param.toString())) {
-                isLimit = true;
-                int limitIndex = params().indexOf(param);
-                offset = convertToLong(params().get(limitIndex + 1).toString());
-                count = convertToLong(params().get(limitIndex + 2).toString());
+            if (IS_BYLEX.equalsIgnoreCase(param.toString())) {
+                isByLex = true;
             }
         }
     }
