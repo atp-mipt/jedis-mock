748a23aafb71da63a2e0a160bb7cf8ed86bb9f3b
Ivan Ponomarev <iponomarev@mail.ru>
2023-04-13 00:24:58 +0300

    mock object encoding

--- native_tests/jm-test-server/src/main/java/com/github/fppt/jedismock/InterceptorMockServer.java
+++ native_tests/jm-test-server/src/main/java/com/github/fppt/jedismock/InterceptorMockServer.java
@@ -49,6 +49,21 @@ public final class InterceptorMockServer {
                                 Slice.create("DEBUG OBJECT command for this data structure is not yet supported")
                         );
                     } else if ("object".equalsIgnoreCase(roName)
+                            && "encoding".equalsIgnoreCase(params.get(0).toString())
+                    ) {
+                        // Handling unsopported OBJECT ENCODING command
+                        RMDataStructure value = state.base().getValue(params.get(1));
+
+                        // Currently it is supported only for RMHash.
+                        // If you will add support for other data structures, insert their handling here
+                        if (value instanceof RMHash) {
+                            RMHash hash = (RMHash) value;
+                            return Response.bulkString(Slice.create("hashtable"));
+                        }
+                        return Response.bulkString(
+                                Slice.create("OBJECT ENCODING command for this data structure is not yet supported")
+                        );
+                    } else if ("object".equalsIgnoreCase(roName)
                             && "refcount".equalsIgnoreCase(params.get(0).toString())
                     ) {
                         //Imitate shared objects
