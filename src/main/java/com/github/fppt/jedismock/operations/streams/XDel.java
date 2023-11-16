65aa4498875d8af6e86229f523123d8008c44bf7
Iван <kalinin.is@phystech.edu>
2023-11-16 11:56:16 +0300

    [streams] XDEL preview

---
+++ src/main/java/com/github/fppt/jedismock/operations/streams/XDel.java
@@ -0,0 +1,41 @@
+package com.github.fppt.jedismock.operations.streams;
+
+import com.github.fppt.jedismock.datastructures.Slice;
+import com.github.fppt.jedismock.datastructures.streams.LinkedMap;
+import com.github.fppt.jedismock.operations.AbstractRedisOperation;
+import com.github.fppt.jedismock.operations.RedisCommand;
+import com.github.fppt.jedismock.server.Response;
+import com.github.fppt.jedismock.storage.RedisBase;
+
+import java.util.List;
+
+/**
+ * XLEN key
+ */
+@RedisCommand("xdel")
+public class XDel extends AbstractRedisOperation {
+    XDel(RedisBase base, List<Slice> params) {
+        super(base, params);
+    }
+
+    @Override
+    protected Slice response() {
+        if (params().size() < 2) {
+            return Response.error("wrong number of arguments for 'xlen' command");
+        }
+
+        Slice key = params().get(0);
+        LinkedMap<Slice, LinkedMap<Slice, Slice>> map = getStreamFromBaseOrCreateEmpty(key).getStoredData();
+
+        int deletedElementsCount = 0;
+
+        for (int i = 1; i < params().size(); ++i) {
+            if (map.remove(params().get(i)) != null) {
+                ++deletedElementsCount;
+            }
+        }
+
+
+        return Response.integer(deletedElementsCount);
+    }
+}
