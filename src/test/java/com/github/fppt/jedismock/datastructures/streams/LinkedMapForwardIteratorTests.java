6517f3c6b6a29706767dcf21d57e7a3b818b6a8f
Iван <kalinin.is@phystech.edu>
2023-12-04 13:12:55 +0300

    [streams] update forward iterator tests

--- src/test/java/com/github/fppt/jedismock/datastructures/streams/LinkedMapForwardIteratorTests.java
+++ src/test/java/com/github/fppt/jedismock/datastructures/streams/LinkedMapForwardIteratorTests.java
@@ -3,9 +3,11 @@ package com.github.fppt.jedismock.datastructures.streams;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LinkedMapForwardIteratorTests {
     @Test
@@ -16,7 +18,7 @@ public class LinkedMapForwardIteratorTests {
         map.append(2, 3);
         map.append(3, 4);
 
-        LinkedMapForwardIterator<Integer, Integer> it = map.iterator();
+        LinkedMapIterator<Integer, Integer> it = map.iterator();
 
         int iterCount = 0;
 
@@ -40,7 +42,7 @@ public class LinkedMapForwardIteratorTests {
         map.append(4, 5);
         map.append(5, 6);
 
-        LinkedMapForwardIterator<Integer, Integer> it = map.iterator(3);
+        LinkedMapIterator<Integer, Integer> it = map.iterator(3);
 
         int iterCount = 0;
 
@@ -81,6 +83,15 @@ public class LinkedMapForwardIteratorTests {
     }
 
     @Test
+    void emptyMapTest() {
+        LinkedMap<Integer, Integer> map = new LinkedMap<>();
+        LinkedMapIterator<Integer, Integer> it = map.iterator();
+
+        assertEquals(false, it.hasNext());
+        assertThrows(NoSuchElementException.class, it::next);
+    }
+
+    @Test
     void removeTest() {
         LinkedMap<Integer, Integer> map = new LinkedMap<>();
 
@@ -90,7 +101,7 @@ public class LinkedMapForwardIteratorTests {
         map.append(4, 5);
         map.append(5, 6);
 
-        LinkedMapForwardIterator<Integer, Integer> it = map.iterator();
+        LinkedMapIterator<Integer, Integer> it = map.iterator();
 
         int iterCount = 0;
 
@@ -101,13 +112,28 @@ public class LinkedMapForwardIteratorTests {
         while (it.hasNext()) {
             ++iterCount;
             Map.Entry<Integer, Integer> entry = it.next();
-            assertEquals(iterCount + 3, entry.getKey());
-            assertEquals(iterCount + 4, entry.getValue());
+            assertEquals(iterCount + 2, entry.getKey());
+            assertEquals(iterCount + 3, entry.getValue());
         }
 
-        assertEquals(2, iterCount);
+        assertEquals(3, iterCount);
         assertEquals(4, map.size());
-        assertNull(map.get(3));
+        assertNull(map.get(2));
+    }
+
+    @Test
+    void nextWasNotInvokedTest() {
+        LinkedMap<Integer, Integer> map = new LinkedMap<>();
+
+        map.append(1, 2);
+        map.append(2, 3);
+        map.append(3, 4);
+        map.append(4, 5);
+        map.append(5, 6);
+
+        LinkedMapIterator<Integer, Integer> it = map.iterator();
+
+        assertThrows(IllegalStateException.class, it::remove);
     }
 
     @Test
@@ -120,11 +146,13 @@ public class LinkedMapForwardIteratorTests {
         map.append(4, 5);
         map.append(5, 6);
 
-        LinkedMapForwardIterator<Integer, Integer> it = map.iterator();
+        LinkedMapIterator<Integer, Integer> it = map.iterator();
 
         int iterCount = 0;
 
+        it.next();
         it.remove();
+
         assertEquals(4, map.size());
         assertEquals(2, map.getHead());
         assertNull(map.get(1));
