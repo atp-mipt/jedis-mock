5afc1cb505df3d3feace4de91c4016e958a56a8c
Iван <kalinin.is@phystech.edu>
2024-01-01 21:21:52 +0300

    [streams] run win

--- src/test/java/com/github/fppt/jedismock/comparisontests/streams/XReadTests.java
+++ src/test/java/com/github/fppt/jedismock/comparisontests/streams/XReadTests.java
@@ -89,7 +89,7 @@ public class XReadTests {
     void xaddWithDelAndLpushShouldNotAwakeClient(Jedis jedis) throws ExecutionException, InterruptedException {
         Future<?> future = blockingThread.submit(() -> {
             List<Map.Entry<String, List<StreamEntry>>> data = blockedClient.xread(
-                    XReadParams.xReadParams().block(25000),
+                    XReadParams.xReadParams().block(20000),
                     ImmutableMap.of("s", StreamEntryID.LAST_ENTRY)
             );
 
