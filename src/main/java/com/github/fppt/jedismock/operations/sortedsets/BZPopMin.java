a57d143424a1286e32469cdd6d12ecc5e27a1829
Iван <kalinin.is@phystech.edu>
2024-04-21 12:33:23 +0300

    codestyle improvements

--- src/main/java/com/github/fppt/jedismock/operations/sortedsets/BZPopMin.java
+++ src/main/java/com/github/fppt/jedismock/operations/sortedsets/BZPopMin.java
@@ -21,6 +21,6 @@ public class BZPopMin extends BZPop {
     @Override
     protected Slice popper(List<Slice> params) {
         List<Slice> result = new ZPop(base(), params, false).pop();
-        return Response.array(Arrays.asList(Response.bulkString(params.get(0)), result.get(0), result.get(1)));
+        return Response.array(Response.bulkString(params.get(0)), result.get(0), result.get(1));
     }
 }
